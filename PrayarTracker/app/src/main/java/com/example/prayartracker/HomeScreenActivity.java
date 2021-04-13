550aae2f3c19a8dd85335be2402cc38fdfaf4dc6
ssssara1998 <59503719+ssssara1998@users.noreply.github.com>
2021-04-13 13:51:47 +0300

    Update HomeScreenActivity.java

--- PrayarTracker/app/src/main/java/com/example/prayartracker/HomeScreenActivity.java
+++ PrayarTracker/app/src/main/java/com/example/prayartracker/HomeScreenActivity.java
@@ -519,7 +519,8 @@ private void calculatePrayerTimes(Location location){
     private void requestPermissions() {
         ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+                Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+        getLastLocation();
     }
 
     // method to check
